af042256f25d4ea951b37a1cf651f67fb1767b9e
Berno01 <ruedabernardo2@gmail.com>
2025-12-11 13:37:12 -0400

    Mejora de estilos y bug en navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, NavbarComponent],
+  imports: [RouterOutlet, HttpClientModule, NavbarComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  constructor(private router: Router) {}
+
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
+  }
 }
