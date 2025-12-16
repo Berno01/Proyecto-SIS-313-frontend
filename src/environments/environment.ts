7a2a7cf6552bfd2d5bd9f75cbcc3bde5beffcb1d
Berno01 <ruedabernardo2@gmail.com>
2025-12-16 17:28:24 -0400

    Cambio de dominio para produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://difficult-camila-sistemastarija-6d731b45.koyeb.app/api',
+  apiUrl: 'https://tender-dacey-tiendarepuestos-2f0db6bf.koyeb.app/api',
 };
+
